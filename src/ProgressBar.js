fb3dc7e24bd21d0b772bcd951ca46c2017f0859e
Mokhtar <mokhtar.mial@gmail.com>
2016-12-08 12:22:17 +0100

    POC

---
+++ src/ProgressBar.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LinearProgress from 'material-ui/LinearProgress';
+
+export default class ProgressBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      value: 0
+    };
+  }
+
+  componentDidMount() {
+    this.tick();
+    this.timer = setInterval(
+      () => this.tick(),
+      50
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+      value: this.getPercentage()
+    });
+  }
+
+  getPercentage() {
+    return (((this.state.date.getSeconds() / 60) + this.state.date.getMilliseconds() / 1000 / 60) * 100).toFixed(2);
+  }
+
+  render() {
+    return (
+      <div className="progressBarContainer">
+        <span>{this.constructor.name}</span>
+        <span style={{float: "right"}}>{this.state.value}%</span>
+        <MuiThemeProvider>
+          <LinearProgress mode="determinate" value={Number(this.state.value)} style={{width:"100%", margin: "auto"}} />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export class Minute extends ProgressBar {
+  getPercentage() {
+    return (((this.state.date.getSeconds() * 1000 + this.state.date.getMilliseconds()) / 60000) * 100).toFixed(2);
+  }
+}
+
+export class Hour extends ProgressBar {
+  getPercentage() {
+    return (((this.state.date.getMinutes() * 60 + this.state.date.getSeconds()) / 3600) * 100).toFixed(2);
+  }
+}
+
+export class Day extends ProgressBar {
+  getPercentage() {
+    return (((this.state.date.getHours() * 3600 + this.state.date.getMinutes() * 60 + this.state.date.getSeconds()) / (24*3600)) * 100).toFixed(2);
+  }
+}
