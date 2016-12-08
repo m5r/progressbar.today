4824602204fd6e52e6b91f43f086922141ebd39b
unknown <Administrateur@FORMATION.LOCAL>
2016-12-08 14:05:53 +0100

    Fix component name

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -5,6 +5,7 @@ import LinearProgress from 'material-ui/LinearProgress';
 export default class ProgressBar extends Component {
   constructor(props) {
     super(props);
+	this.name = "ProgressBar";
     this.state = {
       date: new Date(),
       value: 0
@@ -37,7 +38,7 @@ export default class ProgressBar extends Component {
   render() {
     return (
       <div className="progressBarContainer">
-        <span>{this.constructor.name}</span>
+        <span>{this.name}</span>
         <span style={{float: "right"}}>{this.state.value}%</span>
         <MuiThemeProvider>
           <LinearProgress mode="determinate" value={Number(this.state.value)} style={{width:"100%", margin: "auto"}} />
@@ -48,18 +49,33 @@ export default class ProgressBar extends Component {
 }
 
 export class Minute extends ProgressBar {
+  constructor(props) {
+    super(props);
+    this.name = "Minute";
+  }
+
   getPercentage() {
     return (((this.state.date.getSeconds() * 1000 + this.state.date.getMilliseconds()) / 60000) * 100).toFixed(2);
   }
 }
 
 export class Hour extends ProgressBar {
+  constructor(props) {
+    super(props);
+    this.name = "Hour";
+  }
+  
   getPercentage() {
     return (((this.state.date.getMinutes() * 60 + this.state.date.getSeconds()) / 3600) * 100).toFixed(2);
   }
 }
 
 export class Day extends ProgressBar {
+  constructor(props) {
+    super(props);
+    this.name = "Day";
+  }
+  
   getPercentage() {
     return (((this.state.date.getHours() * 3600 + this.state.date.getMinutes() * 60 + this.state.date.getSeconds()) / (24*3600)) * 100).toFixed(2);
   }
